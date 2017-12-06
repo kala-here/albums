5481b250e7a1309f1e77a99eeba8d5d41227a417
Kala Wetzler <kala.wetzler@gmail.com>
2017-12-06 12:47:06 -0800

    Create and import header

---
+++ src/components/header.js
@@ -0,0 +1,9 @@
+// Import libraries for making component
+import React from 'react';
+import { Text } from 'react-native';
+
+// Make a component
+const Header = () => {
+  return <Text>Header</Text>;
+};
+// Make the component available to other parts of the app
