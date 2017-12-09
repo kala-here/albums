5ee2d9e8c257692e7439b64f73365af5c953df9b
Kala Wetzler <kala.wetzler@gmail.com>
2017-12-09 14:24:42 -0800

    Add view styling (background)

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,24 @@
 // Import libraries for making component
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 // Make a component
 const Header = () => {
-  const { textStyle } = styles;
-  return <Text style={textStyle}>Header</Text>;
+  const { textStyle, viewStyle } = styles;
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>Header</Text>
+    </View>
+  );
 };
 
 const styles = {
+  viewStyle: {
+    backgroundColor: '#F8F8F8'
+  },
   textStyle: {
     fontSize: 20
   }
-}
+};
 // Make the component available to other parts of the app
+export default Header;
