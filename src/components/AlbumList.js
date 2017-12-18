f69b12da989f81d43c3d9fb42355eeaaefcdaeda
Kala Wetzler <kala.wetzler@gmail.com>
2017-12-18 12:02:08 -0800

    Add notes for componentWillMount

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
+// creating a class allows for lifecycle methods
 class AlbumList extends Component {
+  // componentWillMount runs right before content is loaded to the string
+  // ^this makes componentWillMount the perfect place to call for needed data
   componentWillMount() {
     console.log('componentWillMount in AlbumsList');
   }
