13442d34a770940283a0da5f6ef0495376ed944d
Kala Wetzler <kala.wetzler@gmail.com>
2017-12-18 11:48:05 -0800

    Proper format of the simple class component

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -2,14 +2,13 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 class AlbumList extends Component {
-  render(
-
-  )
-  return (
-    <View>
+  render() {
+    return (
+      <View>
       <Text>Album List!!!</Text>
-    </View>
-  );
-};
+      </View>
+    );
+  }
+}
 
 export default AlbumList;
